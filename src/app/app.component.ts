0cedc9679394fa426d098e26b0526a83bdf22a54
Mihai Ureche <mihai.ureche@ntgfreight.com>
2021-09-10 18:29:30 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pw2021-demo';
+}
